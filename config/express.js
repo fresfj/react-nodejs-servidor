767bb7531ba2ab3a4d547707853f69114bfe4cb9
Francisco Freitas Jr <fresfj@gmail.com>
2023-05-01 22:23:13 -0300

    config

--- config/express.js
+++ config/express.js
@@ -6,6 +6,7 @@ const consign = require('consign');
 
 module.exports = () => {
   const app = express();
+  
   app.set('port', process.env.PORT || config.get('server.port'));
 
   app.use(bodyParser.json());
